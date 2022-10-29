5f09caac3cbbafc8798ac625b0197daae9876084
tmcorbin96 <115915843+tmcorbin96@users.noreply.github.com>
2022-10-29 14:10:51 -0400

    I have added to the project

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -14,4 +14,30 @@ public class MainApplication {
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
     }
+    public double sumTotal(int num1,int num2)
+    {
+        return(num1 + num2);
+    }
+    public double multiTotal(int num1, int num2) {
+        return (num1 * num2);
+    }
+    public double subTotal(int num1, int num2) {
+        return (num1 - num2);
+    }
+    public double diviTotal(int num1, int num2) {
+        return (num1 / num2);
+    }
+    public double squareTotal(int num1) {
+        return(Math.sqrt(num1));
+    }
+    public double expoTotal(int num1, int num2) {
+        return(Math.pow(num1, num2));
+    }
+
+    public double inverse(double num1) {
+        String nums = (String) String.format ("3%.2f", (1 /num1));
+        Double newNum = Double.parseDouble(nums);
+        return (1/ num1);
+    }
+
 }
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,57 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import com.zipcodewilmington.scientificcalculator.MainApplication;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+    public void MainApplicationTest() {
+        MainApplication sureTotal = new MainApplication();
+        Double newTotal = sureTotal.inverse(3);
+        System.out.printf("%.4f",newTotal);
+        assertEquals(newTotal);
+    }
+
+    {
+        MainApplication sureTotal = new MainApplication();
+        Double newExpoTotal = sureTotal.expoTotal(2, 6);
+        System.out.println(newExpoTotal);
+        assertEquals(newExpoTotal);
+    }
+
+    {
+        MainApplication sureTotal = new MainApplication();
+        Double newSquareTotal = sureTotal.squareTotal(3);
+        System.out.printf("%.1f",newSquareTotal);
+        assertEquals(newSquareTotal);
+    }
+
+    {
+        MainApplication sureTotal = new MainApplication();
+        Double newMultiTotal = sureTotal.multiTotal(4, 6);
+        System.out.println(newMultiTotal);
+        assertEquals(newMultiTotal);
+    }
+
+    {
+        MainApplication sureTotal = new MainApplication();
+        Double newDiviTotal = sureTotal.diviTotal(18, 3);
+        System.out.println(newDiviTotal);
+        arrertEquals(newDiviTotal);
+    }
+
+    {
+        MainApplication sureTotal = new MainApplication();
+        Double newSubTotal = sureTotal.subTotal(56, 23);
+        System.out.println(newSubTotal);
+        assertEquals(newSubTotal);
+    }
+
+    {MainApplication sureTotal = new MainApplication();
+        Double newSumTotal = sureTotal.sumTotal(343, 943);
+        System.out.println(newSumTotal);
+        assertEquals(newSumTotal);
+    }
+
 }
