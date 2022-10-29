b248a2e20b911b621f6db44989f8ffb918e91fae
zprongua <115650795+zprongua@users.noreply.github.com>
2022-10-29 15:00:21 -0400

    Display changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,17 +1,62 @@
 package com.zipcodewilmington.scientificcalculator;
-
+import java.text.DecimalFormat;
+import java.util.Scanner;
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
     public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to my calculator!");
+        boolean exit = false;
+        double display = 0;
+        DecimalFormat df = new DecimalFormat("##.#######");
+        double output = 0;
+        double num1D = 0;
+        String num1S = "";
+        double num2D = 0;
+        String num2S = "";
+        while (!exit) {
+            System.out.println("Display: " + df.format(display));
+            System.out.print("Number: ");
+            num1S = in.nextLine();
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+            //num1D = Double.parseDouble(num1S);
+
+            System.out.print("Operand: ");
+            String o = in.nextLine();
+            if (!o.equals("cos") && !o.equals("sin") && !o.equals("square")) {
+                System.out.print("Number: ");
+                num2S = in.nextLine();
+                num2D = Double.parseDouble(num2S);
+            }
+            output = interperetOperand(num1D, num2D, o);
+
+
+            display = output;
+            if (num1S.equals("exit")) {
+                break;
+            }
+        }
+    }
+
+    private static Double interperetOperand(double num1, double num2, String o) {
+        double result = 0;
+        if (o.equals("+")) {
+            result = sumTotal(num1, num2);
+        }
+        if (o.equals("square")) {
+            result = square(num1);
+        }
+
+        return result;
+    }
+    private static double sumTotal(double num1, double num2) {
+        double sum = num1 + num2;
+        return sum;
+    }
+    private static double square(double num1) {
+        double square = num1 * num1;
+        return square;
     }
 }
